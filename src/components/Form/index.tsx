dae2faf5d20189ba286eebabb8a5b19a7475f2be
Patrick Casimiro <patrickcasimiroz@gmail.com>
2024-03-05 19:06:45 -0300

    feat: criando e aplicando as action, reducer do novo contato

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,4 +1,4 @@
-import { FormEvent } from 'react'
+import { FormEvent, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
@@ -6,16 +6,29 @@ import { ContactContainer, MainContainer } from '../../styles'
 import { SaveButton } from '../Buttons/styles'
 import * as S from './styles'
 import { setUserInfo } from '../../store/reducers/infoUser'
-import { UserData } from '../../store/reducers/fakeAPI'
 
-const Form = ({ UserData }: { UserData: UserData }) => {
+const Form = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: ''
+  })
+
   const addContact = (e: FormEvent) => {
     e.preventDefault()
-    dispatch(setUserInfo(UserData[1]))
+    dispatch(setUserInfo(formData))
     navigate('/')
+  }
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setFormData({
+      ...formData,
+      [name]: value
+    })
   }
 
   return (
@@ -24,11 +37,29 @@ const Form = ({ UserData }: { UserData: UserData }) => {
         <S.Form onSubmit={addContact}>
           <S.NewContactTitel>Informações do contato</S.NewContactTitel>
           <S.Label htmlFor="inputName">Name:</S.Label>
-          <S.Input type="text" id="inputName" />
+          <S.Input
+            type="text"
+            id="inputName"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+          />
           <S.Label htmlFor="inputEmail">Email:</S.Label>
-          <S.Input type="email" id="inputEmail" />
+          <S.Input
+            type="email"
+            id="inputEmail"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
           <S.Label htmlFor="inputPhone">Phone:</S.Label>
-          <S.Input type="number" id="inputPhone" />
+          <S.Input
+            type="tel"
+            id="inputPhone"
+            name="phone"
+            value={formData.phone}
+            onChange={handleInputChange}
+          />
           <SaveButton type="submit" value="Save" />
         </S.Form>
       </ContactContainer>
--- src/store/reducers/infoUser.ts
+++ src/store/reducers/infoUser.ts
@@ -1,21 +1,25 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
-import { UserData } from './fakeAPI'
-
-interface UserState {
-  userData: UserData[]
+type ContactState = {
+  name: string
+  email: string
+  phone: string
 }
 
-const initialState: UserState = {
-  userData: []
+const initialState: ContactState = {
+  name: '',
+  email: '',
+  phone: ''
 }
 
 const newUserDataSlice = createSlice({
   name: 'newUserData',
   initialState,
   reducers: {
-    setUserInfo(state, action: PayloadAction<UserData>) {
-      state.userData.push(action.payload)
+    setUserInfo(state, action: PayloadAction<ContactState>) {
+      state.name = action.payload.name
+      state.email = action.payload.email
+      state.phone = action.payload.phone
     }
   }
 })
